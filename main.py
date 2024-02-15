84627bc6fb214c4bda3a8afe992abd558927c62b
united2300 <73735743+united2300@users.noreply.github.com>
2024-02-15 19:24:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# World game thing
-
-from VarAndDefs import *
-
-Dungeon = Dungeon(1)
-Dungeon.spawn_enemy()
-Calculate_damage("Atk", p, e)
-print ("hi")
-input("")
+try:
+  # World game thing
+  
+  from VarAndDefs import *
+  
+  Dungeon = Dungeon(1)
+  e = Dungeon.spawn_enemy()
+  Calculate_damage("Atk", p, e)
+  print ("hi")
+  input("")
+except Exception as error:
+  print (error)
